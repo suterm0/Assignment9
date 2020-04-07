6426e91951615caf68417a4af8ada6aa7a993a1c
Michael Suter <suterm@hartwick.edu>
2020-04-07 17:59:30 -0400

    some errors, touching up rn

--- Assignment10.py
+++ Assignment10.py
@@ -2,34 +2,46 @@
 # Assignment 9 & 10 - checking to see if a string is the same reversable
 # 3/31/20
 
+print("Type your word to see if it is a palindrome")
+punctuations = ''' !/?@#$;,()-[]{}<>./%^:'"&*_~ '''
+
+def choice():
+    answer = input("1 to check if palindrome, 2 to exit.>")
+    while answer != 1 or 2:
+        choice()
+    if answer == 1:
+        punc_string = input("Enter your word>")
+        return punc_string
+    else:
+        if answer == 2:
+            exit()
+
 
 def punctuation_check(punc_string):
     string = ''
+    if punc_string.isalpha():
+        for char in punc_string:
+            string += char
+            return palindrome_check(string)
     if not punc_string.isalpha():
         for char in punc_string:
             if char.isalpha():
                 string += char
-    print(string)
-    return string
+        print(string)
+        return palindrome_check(string)
 
 
 def palindrome_check(string):
     string = string.lower()
     length = int(len(string))   # gets the number of characters in the string
-    if length == 0 or length == 1:
-        return print("This is a palindrome,", True)
+    if length <= 1:
+        return print(True, "This is a palindrome")
     elif string[0] != string[-1]:  # if first [0] and last letter [`]are not the same then its not a palindrome
-        return print("This is not a palindrome,", False)
+        return print(False, "This is a palindrome")
     else:
-        return palindrome_check(string[1:-1]))
+        return palindrome_check(string)
 
-# You need to get the input from the user in a loop so it can happen multiple times    
-print("Type your word to see if it is a palindrome")
-punc_string = input(">")
-punctuations = ''' !/?@#$;,()-[]{}<>./%^:'"&*_~ '''
- 
-    
-# Your punctuation check returns a value
-value = punctuation_check(punc_string)
-result = palindrome_check(value)
-print(result)
+
+
+punctuation_check()
+
